5a25f5b0821fe9fab15117b46b3caaaf0cd4bc14
Jade Chi-yen <chiyenjade@gmail.com>
2022-12-08 18:19:01 +0100

    UPD index.php

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 	<main>
 		Leclerc Noel
+		La grande épopée de Noel 
+
+		<nav>
+			La chambre du Père Noel
+			Le salon 
+			Le bureau 
+			La cuisine
+			L'usine
+			La cantine des Lutins
+			L'enclos
+		</nav>
 	</main>
 
 <?php get_footer(); ?>
