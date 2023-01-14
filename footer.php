f0a96ae42f58fb9397e09d3b75734015b9f00898
EmeryDcs <emery.descours@gmail.com>
2023-01-14 13:20:37 +0100

    UPD_%reductionJouetMagasin

--- footer.php
+++ footer.php
@@ -15,19 +15,26 @@
 
         echo '<script> const repertoire = "'.get_template_directory_uri().'"; </script>';
 
-        echo '
-        <script src="'.get_template_directory_uri().'/js/src/class.ObstaclePot.js"></script>
-        ';
-        echo '<script src="'.get_template_directory_uri().'/js/src/jeux/jeu_jour'.$jour.'.js"></script>';
-        
-        if (isset($_POST['reussiteJeu'])){
-            $_SESSION['jour'.$jour]=true;
-
-            echo 
-            '<script>
-                let closeCanva =  document.getElementById("divCanva");
-                closeCanva.style.display = "none";
-            </script>';
+        if (!(isset($_COOKIE['jour'.$jour]))){
+            echo '
+            <script src="'.get_template_directory_uri().'/js/src/class.ObstaclePot.js"></script>
+            ';
+            echo '<script src="'.get_template_directory_uri().'/js/src/jeux/jeu_jour'.$jour.'.js"></script>';
+            
+            if (isset($_POST['reussiteJeu'])){
+                unset($_SESSION['jour']);
+                $_COOKIE['jour'.$jour]=true;
+    
+                $rdmJouet = rand(1,3);
+    
+                $_COOKIE['jouet'.$jour] = get_template_directory_uri().'/images/jpg/jouet'.$rdmJouet.'.jpg';
+    
+                echo 
+                '<script>
+                    let closeCanva =  document.getElementById("divCanva");
+                    closeCanva.style.display = "none";
+                </script>';
+            }
         }
     }
 ?>
