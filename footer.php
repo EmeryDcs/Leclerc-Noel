5d171882bbaa3bfd8daf8287b219d8a5d5272c51
EmeryDcs <emery.descours@gmail.com>
2023-01-15 18:28:12 +0100

    TMP_TestPourJade

--- footer.php
+++ footer.php
@@ -18,21 +18,48 @@
 
         echo '<script> const repertoire = "'.get_template_directory_uri().'"; </script>';
 
-        if (!(isset($_COOKIE['jour'.$jour]))){
-            echo '<script src="'.get_template_directory_uri().'/js/src/class.ObstaclePot.js"></script>';
-            echo '<script src="'.get_template_directory_uri().'/js/src/jeux/jeu_jour'.$jour.'.js"></script>';
-            
-            if (isset($_POST['reussiteJeu'])){
-                unset($_SESSION['jour']);
-                $_SESSION['jour'.$jour]=true;
-    
-                $rdmJouet = rand(1,3);
-    
-                $_SESSION['jouet'.$jour] = get_template_directory_uri().'/images/jpg/jouet'.$rdmJouet.'.jpg';
+        echo get_home_url();
 
-                echo $_COOKIE['jouet'.$jour];
+        switch ($jour){
+            case 4: 
+                if (get_permalink()==get_home_url().'/cantine')
+                    afficheJeu();
+                break;
+            case 7: 
+                if (get_permalink()==get_home_url().'/bureau')
+                    afficheJeu();
+                break;
+            case 10: 
+                if (get_permalink()==get_home_url().'/usine')
+                    afficheJeu();
+                break;
+            case 16:
+                if (get_permalink()==get_home_url())
+                    afficheJeu(); 
+                break;
+            case 22: 
+                if (get_permalink()==get_home_url().'/salon')
+                    afficheJeu();
+                break;
+        }
+
+        function afficheJeu(){
+            if (!(isset($_COOKIE['jour'.$jour]))){
+                echo '<script src="'.get_template_directory_uri().'/js/src/class.ObstaclePot.js"></script>';
+                echo '<script src="'.get_template_directory_uri().'/js/src/jeux/jeu_jour'.$jour.'.js"></script>';
+                
+                if (isset($_POST['reussiteJeu'])){
+                    unset($_SESSION['jour']);
+                    $_SESSION['jour'.$jour]=true;
+        
+                    $rdmJouet = rand(1,3);
+        
+                    $_SESSION['jouet'.$jour] = get_template_directory_uri().'/images/jpg/jouet'.$rdmJouet.'.jpg';
     
-		        echo '<meta http-equiv="Refresh" content="0; url='.get_home_url().'">';
+                    echo $_COOKIE['jouet'.$jour];
+        
+                    echo '<meta http-equiv="Refresh" content="0; url='.get_home_url().'">';
+                }
             }
         }
     }
